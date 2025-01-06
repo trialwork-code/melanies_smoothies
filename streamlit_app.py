39eee91edd7e3150b75d1b86815306e0e74e44d7
trialwork-code <patrick.marchand@improving.com>
2025-01-06 09:41:13 -0700

    Update streamlit_app.py
    Added explicit "data = " parameter to st.dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,5 +61,5 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.text(smoothiefroot_response.json(), use_container_width = True) # smoothiefroot dataframe
+sf_df = st.text(data = smoothiefroot_response.json(), use_container_width = True) # smoothiefroot dataframe
 
