f6f42accc32c219e19de06f6d4ab3687d584a5c1
trialwork-code <patrick.marchand@improving.com>
2025-01-06 09:49:05 -0700

    Update streamlit_app.py
    Relocated import to the top, API call to the for ingredients loop.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session -- removing this line for Streamlit-not-in-Snowflake
 from snowflake.snowpark.functions import col
+import requests
 
 
 # Write directly to the app
@@ -42,6 +43,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True) # smoothiefroot dataframe
 
     # st.write(ingredients_string)
 
@@ -59,8 +62,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order  + '!', icon="✅")
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True) # smoothiefroot dataframe
+
+
 
