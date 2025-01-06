9fa5e54ab43a0fbb6fe8282a2b11041cfe60d663
trialwork-code <patrick.marchand@improving.com>
2025-01-06 09:45:04 -0700

    Update streamlit_app.py
    Switched st.text() to st.dataframe()

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,5 +61,6 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.text(data = smoothiefroot_response.json(), use_container_width = True) # smoothiefroot dataframe
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True) # smoothiefroot dataframe
 
